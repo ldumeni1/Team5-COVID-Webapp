1a628fbed99f53acce56c041aee6b6d5ae6a6a23
ldumeni1 <ldumeni1@umbc.edu>
2022-03-09 17:02:39 -0500

    installed material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,7 @@
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button variant="contained">Hello World</Button>;
 }
 
 export default App;
