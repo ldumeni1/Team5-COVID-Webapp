076c394b41ddcbe2ba8aa1066bd220d92c4ab0dd
Anurag Kunwar <anurag19kunwar@gmail.com>
2022-03-14 10:51:48 -0400

    configured the DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+const cors = require('cors');
+
+const app = express();
+
+// connecting to MySql server
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Anurag#123', // please change this to your password
+  database: 'test_db',
+});
+
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+// required middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// api to insert data into DB
+app.post('/insert', (req, res) => {
+  const uid = req.body.uid;
+  const uname = req.body.uname;
+
+  const sql_ins = "INSERT INTO folks (uid, uname) VALUES(?, ?);"
+  db.query(sql_ins, [uid, uname], (err, result) => {
+    console.log(result);
+  })
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,52 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  //return <Button variant="contained">Hello World</Button>;
+
+ 
+  const [uid, setUId] = useState('');
+  const [uname, setUName] = useState('');
+
+  // this function sends the data to backend
+  const submitForm = () => {
+    Axios.post('http://localhost:3001/insert', {
+      uid: uid, uname: uname,
+    }).then(() => {
+      alert("Successful insert");
+    });
+  };
+
+  return (
+
+    // this is a simple form
+    <div>
+      <div>
+        <h1>This is a test!!!</h1>
+      </div>
+      <form>
+        <div>
+          <label>Id: </label>
+          <input type="text" onChange={(e) => {
+            setUId(e.target.value)
+          }} />
+        </div>
+        <br />
+        <div>
+          <label>Name: </label>
+          <input type="text" onChange={(e) => {
+            setUName(e.target.value)
+          }} />
+        </div>
+        <br />
+
+        <button onClick={submitForm}>Submit</button>
+      </form>
+    </div>
+  );
+
+
 }
 
 export default App;
