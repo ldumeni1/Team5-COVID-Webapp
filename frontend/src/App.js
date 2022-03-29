7a65178ab2a52c3e747280be5e8de93399b1ebe5
ldumenil <ldumenil018@gmail.com>
2022-03-29 11:54:20 -0400

    Leaflet install and demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,22 @@
-import Button from '@mui/material/Button';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useState } from 'react';
 import Axios from 'axios';
 
 function App() {
-  //return <Button variant="contained">Hello World</Button>;
-
- 
-  const [uid, setUId] = useState('');
-  const [uname, setUName] = useState('');
-
-  // this function sends the data to backend
-  const submitForm = () => {
-    Axios.post('http://localhost:3001/insert', {
-      uid: uid, uname: uname,
-    }).then(() => {
-      alert("Successful insert");
-    });
-  };
 
   return (
 
-    // this is a simple form
-    <div>
-      <div>
-        <h1>This is a test!!!</h1>
-      </div>
-      <form>
-        <div>
-          <label>Id: </label>
-          <input type="text" onChange={(e) => {
-            setUId(e.target.value)
-          }} />
-        </div>
-        <br />
-        <div>
-          <label>Name: </label>
-          <input type="text" onChange={(e) => {
-            setUName(e.target.value)
-          }} />
-        </div>
-        <br />
-
-        <button onClick={submitForm}>Submit</button>
-      </form>
-    </div>
+  <MapContainer center={[51.505, -0.09]} zoom={13}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+       A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
 
 
