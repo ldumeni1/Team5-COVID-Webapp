1a8c001e1629229c9c8d73da3a18131a2fe0cf2b
ldumenil <ldumenil018@gmail.com>
2022-03-31 12:59:03 -0400

    Leaflet CSS and GeoJSON

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useState } from 'react';
 import Axios from 'axios';
+import { GeoJSON } from 'react-leaflet';
+
+import counties from "./GeoJson/counties.json"
+
+import "./App.css"
 
 function App() {
 
   return (
-
-  <MapContainer center={[51.505, -0.09]} zoom={13}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-       A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+    <MapContainer center={[-76.9839, 39.0704]} zoom={13}>
+      <GeoJSON style={{fillOpacity: 0}} data={counties.features}/>
+    </MapContainer>
   );
 
 
