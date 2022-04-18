6dab16b58c3e90f36f01caf855094bbb4a5b922d
ldumenil <ldumenil018@gmail.com>
2022-04-18 18:12:08 -0400

    Map Popup with Dummy Data Showing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,32 +11,50 @@ import "./App.css"
 
 function App() {
 
-  const [data, setData] = useState({});
+  const [cases, setCases] = useState(0);
+  const [deaths, setDeaths] = useState(0);
 
   const hoverStyle = {color: "black", weight: 4};
   const defaultStyle = {color: "blue", fillOpacity: 0, weight: 2}
 
-  useEffect(()=>{
-    let nat_data = getData('http://localhost:3001/get_national_level/')
-    setData(nat_data);
-  },[])
+  const [selectedCounty, setSelectedCounty] = useState("Howard");
+  useEffect(async()=>{
+    const response = await getData('http://localhost:3001/get_county_level/3333-01-01/MD/' + selectedCounty)
+    setCases(response.data[0].cl_cases);
+    setDeaths(response.data[0].cl_deaths);
+  },[selectedCounty])
 
-  const displayName = (county, layer) => {
-    layer.bindPopup(data)
+  const [popup, setPopup] = useState(false)
+  const [popupLocation, setPopupLocation] = useState([])
+  const selectCounty = (county, layer) => {
     layer.on({
       mouseover: (event) => {
         event.target.setStyle(hoverStyle);
       },
       mouseout: (event) => {
         event.target.setStyle(defaultStyle)
+      },
+      click: (event) => {
+        setSelectedCounty(county.properties.NAME)
+        setPopupLocation([event.latlng.lat, event.latlng.lng])
+        setPopup(true)
       }
     })
   };
 
   return (
+    popup ? (
+    <MapContainer center={popupLocation} zoom={8}>
+      <Popup position={popupLocation}>
+        <h2>{selectedCounty}</h2>
+        <h4>Number of Cases: {cases}</h4>
+        <h4>Number of Deaths: {deaths}</h4>
+      </Popup>
+      <GeoJSON style={defaultStyle} data={counties.features} onEachFeature={selectCounty} />
+    </MapContainer>) : (
     <MapContainer center={[38.484726, -98.38017]} zoom={5}>
-      <GeoJSON style={defaultStyle} data={counties.features} onEachFeature={displayName} />
-    </MapContainer>
+      <GeoJSON style={defaultStyle} data={counties.features} onEachFeature={selectCounty} />
+    </MapContainer>)
   );
 
 }
--- frontend/src/axois.js
+++ frontend/src/axois.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 //const { response } = require("express");
-const getData = function(url) {
-    axios.get(url).then(response => {return response.data})
+const getData = async (url) => {
+    let data = {}
+    await axios.get(url).then(response => {data = response})
+    return data
 };
 
 export default getData;
